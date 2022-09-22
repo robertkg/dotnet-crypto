74031ab392a271222e0dbaa02d69f9ceb869b86b
robertkg <rgranmo@gmail.com>
2022-09-23 00:01:49 +0200

    Add key derivation and format args

--- run.ps1
+++ run.ps1
@@ -1,5 +1,5 @@
 Write-Output "`n--- Genering keys"
-ssh-keygen -t rsa -b 4096 -C dotnet-crypto-example -N "test123" -m pem -f keys/id_rsa
+ssh-keygen -t rsa -b 4096 -a 100 -C dotnet-crypto -N "test123" -o -m pem -f keys/id_rsa
 ssh-keygen -f keys/id_rsa.pub -m PEM -e > keys/id_rsa.pub.pem
 
 Write-Output "`n--- Running dotnet app"
