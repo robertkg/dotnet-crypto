f5507cf55476772fe24a9e51ae5a52e4bf7e78ba
robertkg <rgranmo@gmail.com>
2022-09-27 21:38:59 +0200

    Add sample file perf tests

--- Program.cs
+++ Program.cs
@@ -1,8 +1,36 @@
 using System.Security.Cryptography;
+using System.Diagnostics;
 using Ionic.Zip;
 
 var rsa = RSA.Create();
 var aes = Aes.Create();
+
+var sampleFileType = Int32.Parse(args[0]);
+var sampleFilePath = "";
+
+switch (sampleFileType)
+{
+    case 1:
+        sampleFilePath = "data/sample_100mb";
+        break;
+    case 2:
+        sampleFilePath = "data/sample_1gb";
+        break;
+    case 3:
+        sampleFilePath = "data/sample_10gb";
+        break;
+    case 4:
+        sampleFilePath = "data/sample_20gb";
+        break;
+    case 5:
+        sampleFilePath = "data/sample_50gb";
+        break;
+    default:
+        Console.WriteLine($"Unknown argument: Expected value in range 1..3, got {sampleFileType}");
+        throw new IndexOutOfRangeException();
+}
+
+Console.WriteLine($"Using sample file {sampleFilePath}");
 
 // Retrieve RSA public key
 var file = File.ReadAllText("keys/id_rsa.pub.pem");
@@ -14,19 +42,32 @@ Console.WriteLine(file);
 aes.GenerateKey();
 var randomKey = aes.Key;
 var randomKeyBase64 = Convert.ToBase64String(randomKey);
-Console.WriteLine($"randomKey:\n\t{randomKeyBase64}");
+Console.WriteLine($"randomKey [Base64]:\n\t{randomKeyBase64}");
 
 // Encrypt random key with RSA public key
 var randomKeyEncrypted = rsa.Encrypt(randomKey, RSAEncryptionPadding.Pkcs1);
 var randomKeyEncryptedBase64 = Convert.ToBase64String(randomKeyEncrypted);
-Console.WriteLine($"randomKeyEncrypted:\n\t{randomKeyEncryptedBase64}");
+Console.WriteLine($"randomKeyEncrypted [Base64]:\n\t{randomKeyEncryptedBase64}");
 
 // Create encrypted archive
-Console.WriteLine("\nEncrypting export.zip with randomKey");
 var zip = new ZipFile();
+var timer = new Stopwatch();
+var zipFileName = "export.zip";
+
+zip.UseZip64WhenSaving = Zip64Option.AsNecessary; // >10 GB sample files will cause Ionic.Zip.ZipException
+
+Console.WriteLine($"\nEncrypting {zipFileName} with randomKey");
 zip.Password = randomKeyBase64;
-zip.AddFile("data/fruits.json");
-zip.Save("export.zip");
+
+Console.WriteLine($"Adding {sampleFilePath}, data/hash.json to archive {zipFileName}");
+zip.AddFile(sampleFilePath);
+zip.AddFile("data/hash.json");
+
+Console.WriteLine($"Creating archive {zipFileName}...");
+timer.Start();
+zip.Save(zipFileName);
+timer.Stop();
+Console.WriteLine($"Completed [Encryption time: {timer.Elapsed.ToString()}]\n");
 
 // Write encrypted random key to file
 var randomKeyFilePath = "random_key.enc";
@@ -34,8 +75,11 @@ Console.WriteLine($"Writing randomKeyEncrypted to {randomKeyFilePath}");
 //File.WriteAllText(randomKeyFilePath, randomKeyEncryptedBase64);
 File.WriteAllBytes(randomKeyFilePath, randomKeyEncrypted);
 
-Console.WriteLine("\nTo decrypt using openssl, run the script:");
+Console.WriteLine("To decrypt using openssl, run the script:");
 Console.WriteLine("\t./openssl_decrypt.ps1");
+Console.WriteLine("\nFinished dotnet-crypto");
+
+/////////////////////////////////////////////////////////////////////////////////////////////////
 
 // Sample code to decrypt randomKey
 // Console.WriteLine("\n\nDecrypting using RSA private key [keys/id_rsa]");
--- run.ps1
+++ run.ps1
@@ -1,9 +1,76 @@
-Write-Output "`n--- Genering keys"
-ssh-keygen -t rsa -b 4096 -a 100 -C dotnet-crypto -N "test123" -o -m pem -f keys/id_rsa
-ssh-keygen -f keys/id_rsa.pub -m PEM -e > keys/id_rsa.pub.pem
+[CmdletBinding()]
+param (
+    [Parameter(Mandatory, HelpMessage = "Sample file to test with. Possible values are:`n1 = 100MB`n2 = 1GB`n3 = 10 GB`n4 = 20 GB`n5 = 50 GB")]
+    [ValidateRange(1, 5)]
+    $SampleFileType
+)
 
-Write-Output "`n--- Running dotnet app"
-dotnet run
+$ErrorActionPreference = 'Stop'
+$dataDir = "$PSScriptRoot\data"
 
-"`n--- Running openssl_decrypt.ps1"
-& $PSScriptRoot/openssl_decrypt.ps1
+Remove-Item $dataDir\sample*
+Remove-Item $dataDir\hash.json -ErrorAction SilentlyContinue
+Remove-Item $PSScriptRoot\export -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item $PSScripRoot\random_key* -ErrorAction SilentlyContinue
+Remove-Item $PSScripRoot\export.zip -ErrorAction SilentlyContinue
+
+# 1 MB   = 1048576 bytes
+# 100 MB = 104857600 bytes
+# 1 GB   = 1073741824 bytes
+# 10 GB  = 10737418240 bytes
+# 100 GB = 107374182400 bytes
+# 1 TB   = 1099511627776 bytes
+# 10 TB  = 10995116277760 bytes
+
+switch ($SampleFileType) {
+    1 { 
+        $sampleFileLength = 104857600
+        $sampleFilePath = "$dataDir\sample_100mb"
+    }
+    2 {
+        $sampleFileLength = 1073741824
+        $sampleFilePath = "$dataDir\sample_1gb"
+    }
+    3 {
+        $sampleFileLength = 10737418240
+        $sampleFilePath = "$dataDir\sample_10gb"
+    }
+    4 {
+        $sampleFileLength = 21474836480
+        $sampleFilePath = "$dataDir\sample_20gb"
+    }
+    5 {
+        $sampleFileLength = 53687091200
+        $sampleFilePath = "$dataDir\sample_50gb"
+    }
+    Default {
+        Write-Error "Unknown SampleFileType $SampleFileType"
+    }
+}
+
+Write-Output 'Creating sample file'
+fsutil file createnew $sampleFilePath $sampleFileLength
+Write-Output 'Computing file hash [SHA1]'
+$sampleFileHash = Get-FileHash -Algorithm SHA1 -Path $sampleFilePath | ConvertTo-Json
+Write-Output $sampleFileHash
+$sampleFileHash > "$dataDir\hash.json"
+
+Write-Output "`nRunning dotnet-crypto"
+dotnet run $SampleFileType
+
+Write-Output "`nDecrypting random_key.enc with RSA private key [keys/id_rsa]"
+openssl rsautl -inkey keys/id_rsa -decrypt -pkcs -in random_key.enc -out random_key.tmp -passin pass:test123
+openssl base64 -in random_key.tmp -out random_key
+Remove-Item random_key.tmp
+$key = Get-Content random_key
+Write-Output "`trandom_key: $key"
+
+$zipFileName = 'export.zip'
+Write-Output "Decrypting $zipFileName with random_key..."
+
+$timer = [System.Diagnostics.Stopwatch]::StartNew()
+& 'C:\Program Files\7-Zip\7z.exe' x -y -oexport -p"$key" export.zip 1>$null
+$timer.Stop()
+
+Write-Output "7z decryption completed with exit code $LASTEXITCODE [Decryption time: $($timer.Elapsed.ToString())]"
+Get-Content -Raw "$PSScriptRoot\export\data\hash.json"
